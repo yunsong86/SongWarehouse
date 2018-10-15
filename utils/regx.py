b4c8f7a0da355e4f7e1a237d84b92544149ac3cd
yunsong86 <yunsong_nuaa@163.com>
2018-10-15 11:53:50 +0800

    regx

---
+++ utils/regx.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-10-15 上午11:43
+# @Author  : YunSong
+# @File    : regx.py
+# @Software: PyCharm
+import re
+
+
+class Regx(object):
+    """
+    正则表达式
+    """
+    regx_chinese = re.compile(r'[\u4e00-\u9fa5]+')
+    regx_brackets = re.compile(r'《[^《》]+》|【[^【】]+】|（[^（）]+）')
+
+
+if __name__ == '__main__':
+    content = '假啊里，jflaj减肥啦放假啊里'
+    for item in re.findall(Regx.regx_chinese, content):
+        print(item)
+
+    content = '假《啊里，jfla》j减【肥啦放假】啊里（肥啦放假）啊里'
+    for item in re.findall(Regx.regx_brackets, content):
+        print(item)
